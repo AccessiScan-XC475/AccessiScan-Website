0efc2f672987d3d0b55c856d132fd157fee83898
Jeffrey <kingtingthegreat@gmail.com>
2024-09-29 14:30:01 -0400

    nextjs to show data from golang

---
+++ components/header.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+const NavLink = ({href, children}: {href: string, children: React.ReactNode}) => (
+    <Link href={href} className="p-1 m-2 text-lg">
+	{children}
+    </Link>
+)
+
+const Header = () => (
+    <header className="flex w-full justify-between items-center my-2 mx-4 py-1 px-4">
+	<Link href="/" className="text-4xl">AccessiScan</Link>
+	<nav>
+	    <NavLink href="/results">Results</NavLink>
+	    <NavLink href="/about">About</NavLink>
+	</nav>
+    </header>
+)
+
+export default Header;
