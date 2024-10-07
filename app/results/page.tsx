f0ff153f2f8324f0f6a778245bd7c1ea837c523b
Josephine Kim <josiek@bu.edu>
2024-10-06 22:10:58 -0400

    added summary

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -1,7 +1,7 @@
 "use client";
+
 import { useState, useEffect } from "react";
 import { BarChart } from '@mui/x-charts/BarChart';
-
 
 type Result = {
   name: string;
@@ -12,21 +12,26 @@ export default function ResultsPage() {
   const [results, setResults] = useState<Result[]>([])
 
   useEffect(() => {
+    // Fetch accessibility selection data from the API
     fetch("/api/accessibility-selection")
       .then(res => res.json())
-      .then(data => setResults(data))
+      .then(data => setResults(data)) // Update state with fetched data
   }, [])
 
   return (
     <div>
       <h3 className="text-center text-2xl">Results</h3>
+      <p className="text-center mb-4">
+        This page displays how often people choose certain accessibility options. We, as a team, are curious to see 
+        which options get chosen the most. We will use this data to improve our platform and the information we offer.
+      </p>
       <div className="w-96 m-12">
         {
           results.length > 0 &&
             <BarChart 
-              dataset={results}
-              series={[{dataKey: "count"}]}
-              xAxis={[{scaleType: "band", dataKey:"name"}]}
+              dataset={results} // Data to be displayed in the chart
+              series={[{dataKey: "count"}]} // Data key for the y-axis
+              xAxis={[{scaleType: "band", dataKey:"name"}]} // Data key for the x-axis
               height={400}
               margin={{ top: 10, bottom: 30, left: 40, right: 10 }}
             />
@@ -35,4 +40,3 @@ export default function ResultsPage() {
     </div>
   )
 }
-
