3fdd5cd0dc9d7afff7946ebf3794ffa22c6e538e
Josephine Kim <josiek@bu.edu>
2024-10-06 22:03:20 -0400

    added major

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,9 @@
-const TeamMember = ({ name, role, age }: { name: string; role: string; age: number }) => (
+const TeamMember = ({ name, role, age, major }: { name: string; role: string; age: number; major: string }) => (
   <div className="p-2 m-6">
     <h4>Hi, I&apos;m {name}</h4>
     <p>I am a {role}</p>
-    <p>I am {age} years old</p> {/* Display the age */}
+    <p>I am {age} years old</p>
+    <p>My major is {major}</p> {/* Display the major */}
   </div>
 );
 
@@ -11,11 +12,11 @@ export default function AboutPage() {
     <div>
       <h2 className="text-center text-2xl">Meet our team!</h2>
       <div className="flex flex-wrap">
-        <TeamMember name="Amelia" role="Product Owner" age={20} />
-        <TeamMember name="Jeffrey" role="Developer" age={31} />
-        <TeamMember name="Hangi" role="Designer" age={23} />
-        <TeamMember name="Ethan" role="Developer" age={20} />
-        <TeamMember name="Josie" role="Developer" age={22} />
+        <TeamMember name="Amelia" role="Product Owner" age={20} major="Computer Science" />
+        <TeamMember name="Jeffrey" role="Developer" age={21} major="Computer Science" />
+        <TeamMember name="Hangi" role="Designer" age={23} major="Graphic Design" />
+        <TeamMember name="Ethan" role="Developer" age={20} major="Computer Science" />
+        <TeamMember name="Josie" role="Developer" age={22} major="Data Science" />
       </div>
     </div>
   );
