4892f369b16ff9524d8695bbc45d6e9988884e39
Jeffrey <kingtingthegreat@gmail.com>
2024-10-14 13:18:07 -0400

    attempting to build out some basic frontend elements

---
+++ components/accessibility-selection.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { MenuItem, Select } from "@mui/material";
+import { useState } from "react";
+
+export default function AccessibilitySelection() {
+  const [selection, setSelection] = useState("");
+
+  return (
+    <div className="flex flex-col items-center">
+      <div className="flex items-center p-1 m-2">
+        <p className="p-1">Choose your goal</p>
+        <Select
+          value={selection}
+          label="Goal"
+          className="w-32 p-0.25"
+          onChange={(e) => setSelection(e.target.value)}
+        >
+          <MenuItem value={"visual"}>Visual</MenuItem>
+          <MenuItem value={"audio"}>Audio</MenuItem>
+          <MenuItem value={"mobility"}>Mobility</MenuItem>
+        </Select>
+      </div>
+      <button
+        onClick={() => {
+          // sent to backend for statistics
+          fetch(`/api/accessibility-selection?name=${selection}`, {
+            method: "POST",
+          });
+        }}
+        className="py-2 px-6 m-4 bg-green-500 rounded-xl"
+      >
+        Go!
+      </button>
+    </div>
+  );
+}
