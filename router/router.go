6ebd1709dd6fa77384f89c808e56200117bc96fa
Jeffrey <kingtingthegreat@gmail.com>
2024-10-15 16:45:18 -0400

    group routes by their functionality

--- router/router.go
+++ router/router.go
@@ -12,11 +12,9 @@ func Router() *http.ServeMux {
 	router.HandleFunc("GET /api/health", handlers.GetHealth)
 
 	router.HandleFunc("GET /api/accessibility-selection", handlers.GetAccessibilitySelections)
-
 	router.HandleFunc("POST /api/accessibility-selection", handlers.PostAccessibilitySelection)
 
 	router.HandleFunc(("GET /api/community-post"), handlers.GetCommunityPost)
-
 	router.HandleFunc(("POST /api/community-post"), handlers.PostCommunityPost)
 
 	return router
