311854a25c56d20470388e87de2a3a6ca3669b66
Jeffrey <kingtingthegreat@gmail.com>
2024-10-28 14:52:19 -0400

    frontned upvoting and downvoting

--- components/community/upvotes-downvotes-display.tsx
+++ components/community/upvotes-downvotes-display.tsx
@@ -1,5 +1,9 @@
+"use client";
 import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt";
 import ThumbDownOffAltIcon from "@mui/icons-material/ThumbDownOffAlt";
+import { useState } from "react";
+import { ThumbDown, ThumbUp } from "@mui/icons-material";
+import { Button } from "@mui/material";
 
 export default function UpvotesDownvotesDisplay({
   upvotes,
@@ -8,12 +12,44 @@ export default function UpvotesDownvotesDisplay({
   upvotes: number;
   downvotes: number;
 }) {
+  const [vote, setVote] = useState<boolean | null>(null);
+
   return (
     <div className="flex items-center">
-      <span className="text-[#1B6AAA] mr-1">{upvotes}</span>
-      <ThumbUpOffAltIcon style={{ color: '#1B6AAA' }} />
-      <span className="text-[#1B6AAA] ml-2">{downvotes}</span>
-      <ThumbDownOffAltIcon style={{ color: '#1B6AAA' }} />
+      <Button
+        sx={{ padding: "0.1rem", width: "1rem" }}
+        onClick={() => {
+          if (vote === true) {
+            setVote(null);
+          } else {
+            setVote(true);
+          }
+        }}
+      >
+        <span className="text-[#1B6AAA] mr-1">{upvotes}</span>
+        {vote === true ? (
+          <ThumbUp sx={{ color: "#1B6AAA" }} />
+        ) : (
+          <ThumbUpOffAltIcon sx={{ color: "#1B6AAA" }} />
+        )}
+      </Button>
+      <Button
+        sx={{ padding: "0.1rem" }}
+        onClick={() => {
+          if (vote === false) {
+            setVote(null);
+          } else {
+            setVote(false);
+          }
+        }}
+      >
+        <span className="text-[#1B6AAA] ml-2">{downvotes}</span>
+        {vote === false ? (
+          <ThumbDown sx={{ color: "#1B6AAA" }} />
+        ) : (
+          <ThumbDownOffAltIcon style={{ color: "#1B6AAA" }} />
+        )}
+      </Button>
     </div>
   );
 }
