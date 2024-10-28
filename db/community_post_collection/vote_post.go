8fc188bdcf40b54ca99ffa9a4510a8380a96071b
Jeffrey <kingtingthegreat@gmail.com>
2024-10-28 17:13:34 -0400

    create functions for upvoting, dowvoting, and removing votes

---
+++ db/community_post_collection/vote_post.go
@@ -0,0 +1,71 @@
+package community_post_collection
+
+import (
+	"AccessiScan-Website/db"
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func UpvotePost(postId primitive.ObjectID, userId primitive.ObjectID) error {
+	collection := db.GetCollection(COMMUNITY_POST_COLLECTION)
+
+	res, err := collection.UpdateByID(context.Background(), postId, bson.M{
+		// add to upvotes
+		"$push": bson.M{"upvoteUsers": userId},
+		// remove from dowvotes
+		"$pull": bson.M{"downvoteUsers": bson.M{"$elemMatch": userId}},
+	})
+	if err != nil {
+		return err
+	}
+
+	if res.ModifiedCount != 1 {
+		return fmt.Errorf("did not update document")
+	}
+
+	return nil
+}
+
+func DownvotePost(postId primitive.ObjectID, userId primitive.ObjectID) error {
+	collection := db.GetCollection(COMMUNITY_POST_COLLECTION)
+
+	res, err := collection.UpdateByID(context.Background(), postId, bson.M{
+		// add to downvotes
+		"$push": bson.M{"downvoteUsers": userId},
+		// remove from upvotes
+		"$pull": bson.M{"downvoteUsers": bson.M{"$elemMatch": userId}},
+	})
+	if err != nil {
+		return err
+	}
+
+	if res.ModifiedCount != 1 {
+		return fmt.Errorf("did not update document")
+	}
+
+	return nil
+}
+
+func RemovePostVote(postId primitive.ObjectID, userId primitive.ObjectID) error {
+	collection := db.GetCollection(COMMUNITY_POST_COLLECTION)
+
+	res, err := collection.UpdateByID(context.Background(), postId, bson.M{
+		// remove from upvotes and dowvotes
+		"$pull": bson.M{
+			"upvoteUsers": bson.M{"$elemMatch": userId}, 
+			"downvoteUsers": bson.M{"$elemMatch": userId},
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	if res.ModifiedCount != 1 {
+		return fmt.Errorf("did not update document")
+	}
+
+	return nil
+}
