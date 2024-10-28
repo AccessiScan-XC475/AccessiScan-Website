68195fe7384c8f78d155b0f8b6a9c2f1fafb474b
Jeffrey <kingtingthegreat@gmail.com>
2024-10-28 14:58:15 -0400

    preview does not need interactive upvote downvote

--- components/community/community-post-preview.tsx
+++ components/community/community-post-preview.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
-import UpvotesDownvotesDisplay from "./upvotes-downvotes-display";
 import ForumIcon from "@mui/icons-material/Forum";
+import { ThumbDown, ThumbUp } from "@mui/icons-material";
 
 export type CommunityPostPreviewProps = {
   id: string;
@@ -20,12 +20,20 @@ export default function CommunityPostPreview({
   return (
     <Link href={`/community/${postPreview.id}`}>
       <div className="bg-[#C7EBD9] rounded-xl p-4 mx-auto my-2 max-w-4xl shadow-lg">
-        <h4 className="text-xl font-semibold text-[#1B6AAA]">{postPreview.title}</h4>
+        <h4 className="text-xl font-semibold text-[#1B6AAA]">
+          {postPreview.title}
+        </h4>
         <p className="text-[#1B6AAA]">{postPreview.content}</p>
-        <UpvotesDownvotesDisplay
-          upvotes={postPreview.upvotes}
-          downvotes={postPreview.downvotes}
-        />
+        <div className="flex items-center text-[#1B6AAA]">
+          <div>
+            <span className="mr-1">{postPreview.upvotes}</span>
+            <ThumbUp sx={{ color: "#1B6AAA" }} />
+          </div>
+          <div>
+            <span className="ml-1">{postPreview.downvotes}</span>
+            <ThumbDown sx={{ color: "#1B6AAA" }} />
+          </div>
+        </div>
         <div className="text-[#1B6AAA]">
           {postPreview.numReplies}
           <ForumIcon />
