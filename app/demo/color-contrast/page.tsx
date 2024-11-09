00fe794e77cf74ac79ece66265eccbd6e8714ccb
Jeffrey <kingtingthegreat@gmail.com>
2024-11-09 12:46:53 -0500

    refactor demo components

--- app/demo/color-contrast/page.tsx
+++ app/demo/color-contrast/page.tsx
@@ -1,4 +1,4 @@
-import PageSection from "@/components/demo/color-contrast-section";
+import ColorContrastSection from "@/components/demo/color-contrast-section";
 
 export default function ColorContrastDemoPage() {
   return (
@@ -12,15 +12,15 @@ export default function ColorContrastDemoPage() {
             contrast with each other.
           </p>
         </div>
-        <PageSection
+        <ColorContrastSection
           heading="sfdoi wekfw"
           content="imetay isyay ayay undamentalfay aspectyay ofyay ouryay experienceyay ofyay ethay orldway , utbay it'syay alsoyay oneyay ofyay ethay ostmay eriousmystay andyay ebatedday enomenaphay inyay othbay iencescay andyay ilosophyphay . onyay ayay asicbay evellay , imetay eemssay straightforward—it’s atwhay eway easuremay ithway ocksclay , ethay easonray eway avehay ayday andyay ightnay , ethay ickingtay ofyay econdssay , inutesmay , andyay ourshay . utbay enwhay ouyay artstay otay igday eeperday , ouyay ealizeray ustjay owhay omplexcay andyay angestray imetay eallyray isyay . in icsphysay , imetay isyay oftenyay eatedtray asyay ayay imensionday . "
         />
-        <PageSection
+        <ColorContrastSection
           heading="wejeck sdkpp"
           content="enthay , there’s ethay ilosophicalphay idesay ofyay imetay . oneyay ofyay ethay entralcay uestionsqay ilosophersphay avehay appledgray ithway isyay etherwhay imetay itselfyay isyay ealray oryay etherwhay it’s ustjay ayay onvenientcay ayway orfay umanshay otay organizeyay eirthay experienceyay ofyay angechay . omesay ilosophersphay argueyay atthay onlyyay ethay esentpray isyay ealray , andyay atthay ethay astpay andyay uturefay areyay eremay onstructscay oryay illusionsyay . othersyay , ownknay asyay eternists ,  elievebay atthay ethay astpay , esentpray , andyay uturefay allyay existyay equally—there’s onay owflay ofyay imetay , ustjay ayay aticstay our-dimensionalfay ockblay erewhay allyay eventsyay areyay aidlay outyay . one ofyay ethay ostmay intriguingyay aspectsyay ofyay imetay isyay ethay ayway umanshay erceivepay ityay . ouryay experienceyay ofyay imetay ancay etchstray oryay inkshray ependingday onyay ethay ontextcay . enwhay e'reway oredbay oryay aitingway orfay omethingsay , imetay eemssay otay agdray . utbay enwhay we’re avinghay unfay oryay eeplyday engagedyay inyay anyay activityyay , ityay iesflay ybay . isthay enomenonphay isyay inkedlay otay ouryay ensesay ofyay attentionyay andyay emorymay . ifyay you’re istractedday andyay otnay ullyfay esentpray , you’re esslay ikelylay otay acktray ethay assagepay ofyay imetay , andyay ityay eemssay otay awlcray . onyay ethay otheryay andhay , enwhay you’re ompletelycay immersedyay , you’re esslay awareyay ofyay ethay ickingtay ockclay , andyay imetay eemssay otay ipslay awayyay unnoticedyay . "
         />
-        <PageSection
+        <ColorContrastSection
           heading="ityay ppowl"
           content="andyay ofyay oursecay , there’s ethay existentialyay imensionday . eway umanshay areyay acutelyyay awareyay ofyay ouryay ownyay initefay iveslay , andyay imetay ecomesbay ayay ourcesay ofyay eaningmay , urgencyyay , andyay anxietyyay . ethay actfay atthay imetay isyay alwaysyay ovingmay orwardfay andyay atthay oneyay ayday we’ll unray outyay ofyay ityay apesshay uchmay ofyay umanhay ulturecay , ilosophyphay , andyay ehaviorbay . omfray ancientyay ologiesmythay otay odernmay ologypsychay , ethay awarenessyay ofyay time’s assingpay ashay inspiredyay ountlesscay eflectionsray onyay ortalitymay , urposepay , andyay egacylay . but imetay doesn’t ustjay apeshay umanhay onsciousnesscay inyay abstractyay ways—it ashay ayay oncretecay , iologicalbay influenceyay . ouryay odiesbay areyay ockedclay otay imetay inyay anymay aysway . orfay instanceyay , eway avehay anyay internalyay ircadiancay mrhythay atthay overnsgay eepslay andyay akeway escyclay , odybay emperaturetay , andyay ormonehay evelslay . isthay mrhythay isyay oughlyray 24 ourshay onglay , influencedyay ybay ethay otationray ofyay ethay earthyay , ichwhay akesmay ityay csynay ithway ethay ay-nightday ecyclay . isruptionsday otay isthay mrhythay , ikelay etjay aglay oryay iftshay orkway , ancay eadlay otay ignificantsay ealthhay oblemspray . imilarlysay , ethay agingyay ocesspray isyay ayay iologicalbay anifestationmay ofyay time—it’s oneyay ofyay ethay ostmay irectday andyay unavoidableyay aysway eway eelfay time’s effectyay onyay ouryay odiesbay . omfray ethay adualgray eclineday inyay ethay efficiencyyay ofyay ouryay organsyay otay ethay icalphysay ignssay ofyay agingyay ikelay inkleswray andyay ayinggray airhay , imetay isyay omethingsay eway can’t escapeyay , evenyay ifyay eway ytray otay owslay ityay ownday . "
         />
