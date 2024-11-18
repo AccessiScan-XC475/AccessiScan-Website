3dd1293310d5700b41a96e686f1b92feea6e7bf7
Jeffrey <kingtingthegreat@gmail.com>
2024-11-18 16:06:17 -0500

    styling

--- components/demo/demo-components.tsx
+++ components/demo/demo-components.tsx
@@ -20,7 +20,7 @@ export default function DemoComponent({
     <>
       <div onClick={handleOpen}>{children}</div>
       <Modal open={open} onClose={handleClose}>
-        <div className="w-52 h-24 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-2 text-center border-2 border-black rounded-xl flex flex-col items-center justify-center">
+        <div className="w-52 p-2 h-28 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white text-center border-2 border-black rounded-xl flex flex-col items-center justify-center">
           <h3 className="text-lg p-1">{label}</h3>
           <input
             className="border-2 w-36"
