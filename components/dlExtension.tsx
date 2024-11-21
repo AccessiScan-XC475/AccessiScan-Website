55a5e62988f0c0a1ecc91cf2723973cb52e9c102
Jeffrey <kingtingthegreat@gmail.com>
2024-11-21 10:36:55 -0500

    make the whole dl extension box a link

--- components/dlExtension.tsx
+++ components/dlExtension.tsx
@@ -1,81 +1,93 @@
-import React from 'react';
-import { Box, Typography } from '@mui/material';
-import iconImage from '../app/icon.png';
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import iconImage from "../app/icon.png";
+import Image from "next/image";
+import Link from "next/link";
 
 const DlExtension: React.FC = () => {
   return (
-    <Box
-      sx={{
-        transform: 'scale(0.7)', // Scale down the entire box to 80% of its original size
-        transformOrigin: 'center', // Scale from the center
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: '-1.5rem',
-        marginBottom: '-2rem',
+    <Link
+      href="https://chromewebstore.google.com/detail/accessiscan-chrome-extens/ahoojddljcjmlgfbbpbbcpgnmjekkafn?authuser=0&hl=en"
+      target="_blank"
+      onMouseOver={() => {
+        const logo = document.getElementById("logo");
+        if (logo) {
+          logo.style.animation = "bounce 0.5s infinite alternate";
+        }
       }}
-    >
-    <Box
-      sx={{
-        textAlign: 'center',
-        padding: '4rem 2.5rem',
-        backgroundColor: '#fbfbfb',
-        borderRadius: '20px',
-        margin: '0 auto',
-        border: '1px solid #d3d3d3',
-        transition: 'box-shadow 0.3s ease-in-out', // Smooth transition for box-shadow
-        "&:hover": {
-          boxShadow: '0px 10px 30px rgba(84, 189, 134, 0.4)', // Shadow appears when hovered
-        },
+      onMouseOut={() => {
+        const logo = document.getElementById("logo");
+        if (logo) {
+          logo.style.animation = "none";
+        }
       }}
+      className="mt-6 mb-8 p-0 flex justify-center items-center scale-75"
     >
-      <Typography variant="h4" component="h2" gutterBottom sx={{ fontFamily: 'inherit', color: '#1B6AAA', fontSize: '2.5rem', fontWeight: '550' }}>
-        Download AccessiScan Chrome Extension
-      </Typography>
-      
-      <Typography variant="body1" paragraph sx={{ fontFamily: 'inherit', fontSize: '1.4rem'}}>
-        Click below to view our Chrome Extension in the Chrome Web Store!
-      </Typography>
-      
       <Box
         sx={{
-          display: 'flex', // Use flexbox to ensure centering
-          justifyContent: 'center', // Horizontal centering
-          alignItems: 'center', // Vertical centering (if needed)
-          marginTop: '2rem',
+          textAlign: "center",
+          padding: "4rem 2.5rem",
+          backgroundColor: "#fbfbfb",
+          borderRadius: "20px",
+          margin: "0",
+          border: "1px solid #d3d3d3",
+          transition: "box-shadow 0.3s ease-in-out", // Smooth transition for box-shadow
+          "&:hover": {
+            boxShadow: "0px 10px 30px rgba(84, 189, 134, 0.4)", // Shadow appears when hovered
+          },
         }}
       >
-        <a href="https://chromewebstore.google.com/detail/accessiscan-chrome-extens/ahoojddljcjmlgfbbpbbcpgnmjekkafn?authuser=0&hl=en" target="_blank" rel="noopener noreferrer">
-          <img
+        <Typography
+          variant="h4"
+          component="h2"
+          gutterBottom
+          sx={{
+            fontFamily: "inherit",
+            color: "#1B6AAA",
+            fontSize: "2.5rem",
+            fontWeight: "550",
+          }}
+        >
+          Download AccessiScan Chrome Extension
+        </Typography>
+
+        <Typography
+          variant="body1"
+          paragraph
+          sx={{ fontFamily: "inherit", fontSize: "1.4rem" }}
+        >
+          Click below to view our Chrome Extension in the Chrome Web Store!
+        </Typography>
+
+        <Box
+          sx={{
+            display: "flex", // Use flexbox to ensure centering
+            justifyContent: "center", // Horizontal centering
+            alignItems: "center", // Vertical centering (if needed)
+            marginTop: "2rem",
+          }}
+        >
+          <Image
+            id="logo"
             src={iconImage.src}
             alt="Download Chrome Extension"
-            style={{
-              width: '100px',
-              cursor: 'pointer',
-              transition: 'transform 0.3s ease-in-out',
-            }}
-            onMouseOver={(e) => {
-              e.currentTarget.style.animation = 'bounce 0.5s infinite alternate';
-            }}
-            onMouseOut={(e) => {
-              e.currentTarget.style.animation = 'none';
-            }}
+            width={100}
+            height={100}
           />
-        </a>
-      </Box>
+        </Box>
 
-      <style jsx>{`
-        @keyframes bounce {
-          0% {
-            transform: translateY(0);
+        <style jsx>{`
+          @keyframes bounce {
+            0% {
+              transform: translateY(0);
+            }
+            100% {
+              transform: translateY(-20px);
+            }
           }
-          100% {
-            transform: translateY(-20px);
-          }
-        }
-      `}</style>
-    </Box> 
-    </Box>
+        `}</style>
+      </Box>
+    </Link>
   );
 };
 
