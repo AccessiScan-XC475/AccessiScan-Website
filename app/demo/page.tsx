5c1e3086d05ac0f676dfe2f497cd95d01fceffad
Jeffrey <kingtingthegreat@gmail.com>
2024-11-21 10:55:38 -0500

    styling

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 
 export default function DemoSelectionPage() {
-  const linkStyles = "hover:underline text-xl";
+  const linkStyles = "hover:underline text-2xl p-3 m-1 transition-all";
   return (
-    <div>
-      <h1 className="text-4xl">What would you like to work on?</h1>
-      <div className="flex flex-col">
+    <div className="flex flex-col items-center m-1 p-6">
+      <h1 className="text-4xl font-medium m-2 p-3 text-[#1B6AAA]">
+        Which type of accessibility would you like to work on?
+      </h1>
+      <div className="grid grid-cols-2">
         <Link href="/demo/color-contrast" className={linkStyles}>
           Color Contrast
         </Link>
