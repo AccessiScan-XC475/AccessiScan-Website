ac888d1509b0233e24bf54519c5365a408da831e
Jeffrey <kingtingthegreat@gmail.com>
2024-11-21 17:06:41 -0500

    get user by secret in middleware

--- middleware/user_info.go
+++ middleware/user_info.go
@@ -12,18 +12,31 @@ func UserInfo(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// get user from db
 		sessionId := cookies.GetSessionId(r)
-		if sessionId == "" {
+		secret := r.URL.Query().Get("secret")
+
+		if sessionId == "" && secret == "" {
+			// no unique id
 			next.ServeHTTP(w, r)
 			return
 		}
 
-		user, err := users_collection.GetUserBySessionId(sessionId)
-		if err != nil {
-			if err.Error() == "redirect" {
-				log.Println("expired sessionId")
+		var user *users_collection.AccessiScanUser
+		var err error
+		if sessionId != "" {
+			user, err = users_collection.GetUserBySessionId(sessionId)
+			if err != nil {
+				if err.Error() == "redirect" {
+					log.Println("expired sessionId")
+				}
+				next.ServeHTTP(w, r)
+				return
 			}
-			next.ServeHTTP(w, r)
-			return
+		} else if secret != "" {
+			user, err = users_collection.GetUserBySecret(secret)
+			if err != nil {
+				next.ServeHTTP(w, r)
+				return
+			}
 		}
 
 		log.Println("got user")
