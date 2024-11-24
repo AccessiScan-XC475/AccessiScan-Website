b53fdaf32e01fa9b40c430c994f5bc5cebbaabf5
amelia-liston <aliston@bu.edu>
2024-11-24 14:47:01 -0500

    Added gradient button for navlinks on demo page

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -7,17 +7,57 @@ export default function DemoSelectionPage() {
       <h1 className="text-4xl font-medium m-2 p-3 text-[#1B6AAA]">
         Which type of accessibility would you like to work on?
       </h1>
-      <div className="grid grid-cols-2">
-        <Link href="/demo/color-contrast" className={linkStyles}>
+      <div className="grid grid-cols-2 gap-4">
+        <Link
+          href="/demo/color-contrast"
+          className={linkStyles}
+          style={{
+            border: "3px solid transparent",
+            borderImage:
+              "linear-gradient(45deg, #1b6aaa, #54bd86, #9b8bf4, #8babf1) 1",
+            borderImageSlice: 1,
+            textAlign: "center",
+          }}
+        >
           Color Contrast
         </Link>
-        <Link href="/demo/text-size" className={linkStyles}>
+        <Link
+          href="/demo/text-size"
+          className={linkStyles}
+          style={{
+            border: "3px solid transparent",
+            borderImage:
+              "linear-gradient(45deg, #1b6aaa, #54bd86, #9b8bf4, #8babf1) 1",
+            borderImageSlice: 1,
+            textAlign: "center",
+          }}
+        >
           Text Size
         </Link>
-        <Link href="/demo/alt-text" className={linkStyles}>
+        <Link
+          href="/demo/alt-text"
+          className={linkStyles}
+          style={{
+            border: "3px solid transparent",
+            borderImage:
+              "linear-gradient(45deg, #1b6aaa, #54bd86, #9b8bf4, #8babf1) 1",
+            borderImageSlice: 1,
+            textAlign: "center",
+          }}
+        >
           Alt Text
         </Link>
-        <Link href="/demo/line-spacing" className={linkStyles}>
+        <Link
+          href="/demo/line-spacing"
+          className={linkStyles}
+          style={{
+            border: "3px solid transparent",
+            borderImage:
+              "linear-gradient(45deg, #1b6aaa, #54bd86, #9b8bf4, #8babf1) 1",
+            borderImageSlice: 1,
+            textAlign: "center",
+          }}
+        >
           Line Spacing
         </Link>
       </div>
