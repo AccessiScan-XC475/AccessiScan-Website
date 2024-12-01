b63f77fda21ef01b400533c480d7528056fe8e7b
amelia-liston <aliston@bu.edu>
2024-12-01 18:06:49 -0500

    Updated text size demo page

--- app/demo/text-size/page.tsx
+++ app/demo/text-size/page.tsx
@@ -1,14 +1,22 @@
 import TextSizeSection from "@/components/demo/text-size-section";
+import Link from "next/link";
+import { ArrowBack } from "@mui/icons-material";
 
 export default function ColorContrastDemoPage() {
   return (
     <div className="w-full flex pb-8">
-      <div className="w-1/5"></div>
+      <div className="w-1/5">
+      <h6 className="mx-8 my-4 ml-2">
+        <Link href="/demo" className="text-[#49A776]">
+          <ArrowBack /> Back to Demo options
+        </Link>
+      </h6></div>
       <div className="w-3/5">
         <div className="text-center p-3">
-          <h1 className="text-5xl p-1 text-black">Text Size Demo</h1>
-          <p className="p-1 text-lg">
+          <h1 className="text-5xl p-1 text-[#1B6AAA] font-semibold">Text Size Demo</h1>
+          <p className="p-1 text-lg text-[#1B6AAA] font-medium">
             Edit the text sizes on this page until all text is adequately sized.
+            Select the text below to access the adjusters.
           </p>
         </div>
         <TextSizeSection
