c65feebe5e8213adeaa424b29aec34945e611c41
amelia-liston <aliston@bu.edu>
2024-12-01 18:00:39 -0500

    Updating demo pages

--- app/demo/color-contrast/page.tsx
+++ app/demo/color-contrast/page.tsx
@@ -5,13 +5,14 @@ import { ArrowBack } from "@mui/icons-material";
 export default function ColorContrastDemoPage() {
   return (
     <div className="w-full flex pb-8">
-      <div className="w-1/5"></div>
-      <div className="w-3/5">
+      <div className="w-1/5">
       <h6 className="mx-8 my-4 ml-2">
         <Link href="/demo" className="text-[#49A776]">
           <ArrowBack /> Back to Demo options
         </Link>
       </h6>
+      </div>
+      <div className="w-3/5">
         <div className="text-center p-3">
           <h1 className="text-5xl p-1 text-[#1B6AAA] font-semibold">Color Contrast Demo</h1>
           <p className="p-1 text-lg text-[#1B6AAA] font-medium">
