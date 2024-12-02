acd64f423325bab429e1ebc645218e7ffba9c1f8
Jeffrey <kingtingthegreat@gmail.com>
2024-12-02 14:27:04 -0500

    insert into db

--- db/feedback_collection/create_feedback_submission.go
+++ db/feedback_collection/create_feedback_submission.go
@@ -1,5 +1,29 @@
 package feedback_collection
 
+import (
+	"AccessiScan-Website/db"
+	"context"
+	"fmt"
+)
+
 func CreateFeedbackSubmission(name, message, email string) error {
+	if name == "" || message == "" {
+		return fmt.Errorf("name or message missing")
+	}
+
+	newFeedbackSubmission := FeedbackSubmission{
+		Name:    name,
+		Message: message,
+		Email:   email,
+	}
+
+	_, err := db.GetCollection(FEEDBACK_SUBMISSION_COLLECTION).InsertOne(
+		context.Background(),
+		newFeedbackSubmission,
+	)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
