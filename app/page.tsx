f67bae1b75fcf1ba1c9edd4c049f1bb0d0755994
amelia-liston <aliston@bu.edu>
2024-12-04 22:09:11 -0500

    Updated colors on home page and in globals

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ import DLExtension from "@/components/dlExtension";
 export default function HomePage() {
   return (
     <div className="flex flex-col items-center px-4 md:px-8 lg:px-16 py-8">
-      <h2 style={{ color: "#54BD86", fontSize: "24pt", fontWeight: 800 }}>
+      <h2 style={{ color: "#1B6AAA", fontSize: "24pt", fontWeight: 800 }}>
         Build a website better, together
       </h2>
-      <h3 style={{ color: "#1B6AAA", fontSize: "16pt" }}>
+      <h3 style={{ color: "#7468B6", fontSize: "16pt" }}>
         Empowering developers for accessible websites
       </h3>
       <DLExtension />
